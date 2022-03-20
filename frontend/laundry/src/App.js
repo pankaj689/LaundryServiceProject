091ff595909df728acd85fc386080b6c09d469d5
yakubk18 <golandazyakub@gmail.com>
2022-03-20 11:36:35 +0530

    Sample

--- frontend/laundry/src/App.js
+++ frontend/laundry/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Login />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
