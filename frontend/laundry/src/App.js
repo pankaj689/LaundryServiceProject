b749fffbe7120ae17d406e25648e32e44f43c8d2
pankaj689 <96818307+pankaj689@users.noreply.github.com>
2022-03-21 10:04:58 +0530

    btnandsignin

--- frontend/laundry/src/App.js
+++ frontend/laundry/src/App.js
@@ -1,25 +1,25 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import Login from "./components/login";
+// import Login from "./components/login";
 
+// import {Ragister,Ragisteruser,SigninReg,Signinhome,Create,Cancel,Process,Reset,Confirm,Go_To_Orders,Cancel_Order} from "./components/button"
+import { Signin } from "./components/signin";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Login />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      {/* <Ragister/>
+      <Ragisteruser/>
+      <SigninReg/>
+      <Signinhome/>
+      <Create/>
+      <Cancel/>
+      <Process/>
+      <Reset/>
+      <Confirm/>
+      <Go_To_Orders/>
+      <Cancel_Order/> */}
+      <Signin/>
     </div>
   );
 }
