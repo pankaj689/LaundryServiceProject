de923df098cd23ef3e549a26690b75b0f3f31e37
Rakesh Kumar <71342146+rakadking@users.noreply.github.com>
2022-03-21 23:46:40 +0530

    Update createorder.js

--- backend/routers/createorder.js
+++ backend/routers/createorder.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router();
 const Createorder = require("../model/orderhistry")
+const summary = require('../model/summery');
+
 var bodyParser = require('body-parser')
 router.use(bodyParser.json())
 router.post("/Createorder", async (req, res) => {
@@ -9,9 +11,18 @@ router.post("/Createorder", async (req, res) => {
         const order = await Createorder.create(
             req.body
         )
+        const summeryData = await summary.create({
+        prodType: req.prodType,
+        quantity: req.quantity,
+        subTotal: req.body.subTotal,
+        total: req.body.total,
+        address: req.body.address,
+        user: req.user
+    })
         res.json({
             status: "success",
-            order
+            order,
+            summeryData
         })
     } catch (e) {
         res.status(400).json({
