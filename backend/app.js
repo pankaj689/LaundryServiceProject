d70ab7093dcc70a8051a615048938b8c4a86c13e
yakubk18 <golandazyakub@gmail.com>
2022-03-21 23:17:36 +0530

    21marbackend-1

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,29 @@
-const express=require("express")
+const express = require("express");
+var mongoose = require("mongoose");
+const app = express();
+const routeLogin = require("./routers/login.js");
+var url =
+  "mongodb+srv://group14:group1410x@laundryservice.m0iy6.mongodb.net/LaundryService?retryWrites=true&w=majority";
 
-var mongoose = require('mongoose');
-const app=express()
-var url = "mongodb+srv://group14:group1410x@laundryservice.m0iy6.mongodb.net/LaundryService?retryWrites=true&w=majority";
-
-mongoose.connect(url, function(err, db) {
+mongoose.connect(url, function (err, db) {
   if (err) throw err;
   console.log("Database created!");
-  db.close();
+  // db.close();
 });
-//svsvsfbsfsbsfvsgot
 
-// const username = "group14";
-// const password = "group14@10x";
-// const cluster = "cluster0.vlyun";
-// const dbname = "Laundryservices";
+app.use("/api", routeLogin);
 
+app.listen(5000, () => {
+  console.log("listening to port 5000");
+});
 
-// mongoose.connect(
-//   `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
-//   {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true
-//   }
-// );
+// app.get("/", (req, res) => {
+//   res.send("Server connected");
+// });
 
-app.listen(5000,()=>{console.log("listening to port 5000")})
+// //------------------------------------Login---------------------------------------------------------------------------
+// app.post("/login", (req, res) => {
+//   userDetails = req.body;
+//   userID = userDetails.userID;
+//   reqPassword = userDetails.password;
+// });
--- backend/routers/first.js
+++
@@ -1,1 +0,0 @@
-console.log("delet file")
