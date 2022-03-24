34effe4dea8a23bce2a8627ed77a0966da4e72d8
yakubk18 <golandazyakub@gmail.com>
2022-03-24 13:00:48 +0530

    createdsummary

--- frontend/laundry/src/App.js
+++ frontend/laundry/src/App.js
@@ -1,16 +1,15 @@
-import UserAccountPage from "./components/useraccount"
+import UserAccountPage from "./components/useraccount";
 // import Footer from "./components/footer.js";
 // import Login from "./components/login.js";
 // import AboutUS from "./components/aboutus";
 
-
-
+import Summary from "./components/summary";
 import Refer from "./components/refer";
 import Register from "./components/register";
 function App() {
   return (
     <div className="App">
- 
+      <Summary />
     </div>
   );
 }
